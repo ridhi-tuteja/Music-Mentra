fe7d51f7eefd34697c2ec8462859e2a89cdac695
Your ridhi-tuteja <ridhituteja@gmail.com>
2025-07-13 03:09:40 +0530

    some changes

--- src/components/ui/background-beams.tsx
+++ src/components/ui/background-beams.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
-import { motion } from "motion/react";
+// import { motion } from "motion/react";
+import { motion } from "framer-motion";
+
 import { cn } from "@/utils/cn";
 
 export const BackgroundBeams = React.memo(
--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/utils/cn";
-import { AnimatePresence, motion } from "motion/react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { useState } from "react";
 
