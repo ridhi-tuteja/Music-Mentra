afe2f4080b8dd0a2af521300139cb225e8862487
Your ridhi-tuteja <ridhituteja@gmail.com>
2025-07-15 02:56:22 +0530

    change to framer-motion

--- src/components/ui/background-gradient.tsx
+++ src/components/ui/background-gradient.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/utils/cn";
 import React from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export const BackgroundGradient = ({
   children,
