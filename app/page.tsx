99312a8aee1caed86b75efe4acce4e46c4298be3
Your ridhi-tuteja <ridhituteja@gmail.com>
2025-07-16 00:38:18 +0530

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
-import TestimonialCards from "@/components/TestimonialCards"
+import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
-import Instructors from "@/components/Instructors"
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <HeroSection/>
-      <FeaturedCourses/>
-      <TestimonialCards/>
-      <UpcomingWebinars/>
-      <Instructors/>
-      <Footer/>
-
+    <main className="bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <section className="min-h-screen">
+        <HeroSection />
+      </section>
+      <section className="min-h-screen">
+        <FeaturedCourses />
+      </section>
+      <section className="min-h-screen">
+        <TestimonialCards />
+      </section>
+      <section className="min-h-screen">
+        <UpcomingWebinars />
+      </section>
+      <section className="min-h-screen">
+        <Instructors />
+      </section>
+      <section className="min-h-screen">
+        <Footer />
+      </section>
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,14 +13,14 @@ function HeroSection() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-        <div className="p-4 relative z-10 w-full text-center" >
+        <div className="p-8 relative z-10 w-full text-center" >
             <h1
             className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
             >Master the art of music</h1>
             <p
             className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
             >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
-            <div className="mt-4">
+            <div className="mt-8">
                 <Link href={"/courses"}>
                     <Button
                     borderRadius="1.75rem"
